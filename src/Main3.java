58c3a24344490bacf4d10707e5512c33117b8953
gyeyoung7962 <gaeyoung7962@naver.com>
2024-02-19 20:06:43 +0900

    처음 만들어본 자바 코드

---
+++ src/Main3.java
@@ -0,0 +1,9 @@
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println("밥먹자");
+        System.out.println("돈까스");
+        System.out.println("햄버거");
+        System.out.println("피자");
+        System.out.println("제육볶음");
+    }
+}
