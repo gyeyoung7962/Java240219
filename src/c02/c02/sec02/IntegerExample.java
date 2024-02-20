d87190163c48c3c4f1cde3cbc1bbbe106e9d964e
gyeyoung7962 <gaeyoung7962@naver.com>
2024-02-20 10:02:27 +0900

    long

---
+++ src/c02/c02/sec02/IntegerExample.java
@@ -0,0 +1,18 @@
+package c02.c02.sec02;
+
+public class IntegerExample {
+    public static void main(String[] args) {
+
+        int var1 = 0b1011; //2진법 0b로 시작
+        int var2 = 0206; //8진법 0으로 시작
+        int var3 = 2982; //10진법
+        int var4 = 0xb3; //16진법 0x로 시작
+
+        //영문자는 대소문자 구분 안함
+        System.out.println("var1:"+var1);
+        System.out.println("var2:"+var2);
+        System.out.println("var3:"+var3);
+        System.out.println("var4:"+var4);
+
+    }
+}
